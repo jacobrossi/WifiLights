7ecbbec8cb86a629ce1a0c5031a6ae9da57cd9d0
jacobrossi <gtjrossi@gmail.com>
2017-12-11 00:15:15 -0800

    Switch to persistent file storage so settings survive restart
    Fix default color1
    
    Factor client script into separate file, enable eslint
    
    Actually enable eslint

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,6 @@
 var express = require('express');
+var storage = require('node-persist');
 var router = express.Router();
-
-require.main.currentPattern = "1";
-require.main.startTime = new Date(2016,1,1,16,30,0,0);
-require.main.stopTime = new Date(2016,1,1,0,15,0,0);
-require.main.color1 = 255; //Blue
-require.main.color2 = 65280; //Green
 
 /* GET Read current pattern */
 router.get('/:id?', function(req, res, next) {
@@ -19,49 +14,52 @@ router.get('/:id?', function(req, res, next) {
 
 
     if (id) {
-        require.main.currentPattern = id;
+        storage.setItemSync('currentPattern',id);
         console.log("Pattern changed to " + id);
     }
     if (start) {
         [hours, min] = start.split(":",2);
-        require.main.startTime = new Date(2016,1,1,hours,min,0,0);
-        console.log("Start time changed to " + require.main.startTime);
+        storage.setItemSync('startTime',new Date(2016,1,1,hours,min,0,0));
+        console.log("Start time changed to " + storage.getItemSync('startTime'));
     }
     if (stop) {
         [hours, min] = stop.split(":",2);
-        require.main.stopTime = new Date(2016,1,1,hours,min,0,0);
-        console.log("Stop time changed to " + require.main.stopTime);
+        storage.setItemSync('stopTime',new Date(2016,1,1,hours,min,0,0));
+        console.log("Stop time changed to " + storage.getItemSync('stopTime'));
     }
     if (color1) {
-        require.main.color1 = parseInt(color1);
+        storage.setItemSync('color1',parseInt(color1));
     }
     if (color2) {
-        require.main.color2 = parseInt(color2);
+        storage.setItemSync('color2',parseInt(color2));
     }
     if (brightness) {
-        require.main.brightness = parseInt(brightness);
+        storage.setItemSync('brightness',parseInt(brightness));
     }
-    result.brightness = require.main.brightness;
-    result.color1 = require.main.color1;
-    result.color2 = require.main.color2;
+    result.brightness = storage.getItemSync('brightness');
+    result.color1 = storage.getItemSync('color1');
+    result.color2 = storage.getItemSync('color2');
 
     var currentDate = new Date();
     var currentTime = new Date(2016,1,1,currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds(), currentDate.getMilliseconds());
+    var tStart = new Date(storage.getItemSync('startTime'));
+    var tStop = new Date(storage.getItemSync('stopTime'));
+
     //If the start time is less than the stop time, then we want the current time to be inside them. If not, we want the current time to be outside them.
-    if( ((require.main.startTime < require.main.stopTime) && (currentTime > require.main.startTime && currentTime < require.main.stopTime)) || 
-        ((require.main.stopTime < require.main.startTime) && (currentTime > require.main.startTime || currentTime < require.main.stopTime)) )
+    if( ((tStart < tStop) && (currentTime > tStart && currentTime < tStop)) ||
+        ((tStop < tStart) && (currentTime > tStart || currentTime < tStop)) )
         {
-            result.pattern = parseInt(require.main.currentPattern);
+            result.pattern = parseInt(storage.getItemSync('currentPattern'));
             console.log("Inside operating hours.");
-            console.log("START:" + require.main.startTime.getTime());
-            console.log("STOP:" + require.main.stopTime.getTime());
+            console.log("START:" + tStart.getTime());
+            console.log("STOP:" + tStop.getTime());
             console.log("NOW:"+currentTime.getTime());
             console.log("Current pattern: " + result.pattern);
     } else {
             result.pattern = 0;
             console.log("Outside operating hours.");
-            console.log("START:" + require.main.startTime.getTime());
-            console.log("STOP:" + require.main.stopTime.getTime());
+            console.log("START:" + tStart.getTime());
+            console.log("STOP:" + tStop.getTime());
             console.log("NOW:"+currentTime.getTime());
     }
     res.send(JSON.stringify(result))
